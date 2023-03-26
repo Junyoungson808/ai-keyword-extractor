05816b4a037476b4ce2a4e73212a89b93c48809e
Junyoung Son <junyoungson808@gmail.com>
2023-03-26 03:31:58 +0000

    await functions created

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const extractKeywords = (text) => {
+  const extractKeywords = async (text) => {
     // console.log(text);
     setLoading(true);
     setIsOpen(true);
@@ -22,9 +22,22 @@ const App = () => {
       },
       body: JSON.stringify({
         model: 'text-davinci-003',
-        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' + text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8
       })
     }
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
+
   };
 
   return (
