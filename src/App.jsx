91e02b85ef0df0778eec9ca62024cc9f24a79c67
Junyoung Son <junyoungson808@gmail.com>
2023-03-26 03:24:01 +0000

    open api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { useState } from 'react'
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Textinput from "./components/Textinput";
 
 const App = () => {
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const extractKeywords = (text) => {
-    console.log(text);
+    // console.log(text);
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas',
+      })
+    }
   };
 
   return (
