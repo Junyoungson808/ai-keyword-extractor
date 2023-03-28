1d1958045cbc873dadc8dcfc4f1f45991a5b77c4
Junyoung Son <junyoungson808@gmail.com>
2023-03-28 05:17:22 +0000

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Textinput from "./components/Textinput";
+import KeywordsModal from "./components/KeywordsModal";
 
 const App = () => {
-  const [keywords, setKeywords] = useState('');
+  const [keywords, setKeywords] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -15,19 +16,22 @@ const App = () => {
     setIsOpen(true);
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' + text + '',
+        model: "text-davinci-003",
+        prompt:
+          "Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n" +
+          text +
+          "",
         temperature: 0.5,
         max_tokens: 60,
-        frequency_penalty: 0.8
-      })
-    }
+        frequency_penalty: 0.8,
+      }),
+    };
 
     const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
 
@@ -35,9 +39,13 @@ const App = () => {
 
     const data = json.choices[0].text.trim();
     console.log(data);
+
     setKeywords(data);
     setLoading(false);
+  };
 
+  const closeModal = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -47,6 +55,12 @@ const App = () => {
         <Textinput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   );
 };
